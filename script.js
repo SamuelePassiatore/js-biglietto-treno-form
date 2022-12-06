e4d2a68151aa592618f711f8516f78d8f3cea4a3
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-06 16:28:08 +0100

    added event listener click to buttons

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 
 /* 
 1. Prendo gli elementi in pagina.
-2. Calcolo il prezzo totale del viaggio.
-3. Applico uno sconto del 20% ai minorenni (<18) e uno del 40% agli over 65 (>= 65).
+2. Aggancio ai button un event listener.
+3.
+4. Calcolo il prezzo totale del viaggio.
+5. Applico uno sconto del 20% ai minorenni (<18) e uno del 40% agli over 65 (>= 65).
 4.
 
 */
-
 
 // 1 - Recupero gli elementi in pagina
 const inputName = document.getElementById('name');
@@ -24,7 +25,41 @@ const inputKm = document.getElementById('km');
 const inputAge = document.getElementById('age');
 const btnCreate = document.getElementById('btn-create');
 const btnReset = document.getElementById('btn-reset');
-const ticketPrice = document.getElementById('ticket-price');
+let ticketPrice = document.getElementById('ticket-price');
+
+// 2 - Aggancio ai button un event listener.
+btnCreate.addEventListener('click', function () {
+  // const name user
+  const name = inputName.value;
+  console.log(name);
+
+  // const km user
+  const km = parseInt(inputKm.value.trim());
+  console.log(km);
+
+  // const age user 
+  const age = parseInt(inputAge.value.trim());
+  console.log(age);
+});
+
+btnReset.addEventListener('click', function () {
+  // const name user
+  inputName.value = '';
+
+  // const km user
+  inputKm.value = '';
+  
+  // const age user 
+  inputAge.value = '';
+});
+
+
+
+
+
+
+
+
 
 
 
