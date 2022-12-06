0c862ef19a7404c5dc47c06eeae63eadf8979eb6
SamuelePassiatore <semmisno2@protonmail.com>
2022-12-06 15:32:41 +0100

    recover the element in page

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ Sulla base di queste informazioni dovrà calcolare il prezzo totale del bigliett
 - va applicato uno sconto del 20% per i minorenni
 - va applicato uno sconto del 40% per gli over 65.
 */
+
+/* 
+1. Prendo gli elementi in pagina.
+2. Calcolo il prezzo totale del viaggio.
+3. Applico uno sconto del 20% ai minorenni (<18) e uno del 40% agli over 65 (>= 65).
+4.
+
+*/
+
+
+// 1 - Recupero gli elementi in pagina
+const inputName = document.getElementById('name');
+const inputKm = document.getElementById('km');
+const inputAge = document.getElementById('age');
+const btnCreate = document.getElementById('btn-create');
+const btnReset = document.getElementById('btn-reset');
+const ticketPrice = document.getElementById('ticket-price');
+
+
+
+
+
+
+
